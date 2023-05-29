fb7c398ed5a57a8b476326698ef6738a773fb77d
Jeremy <Jeremy.Y.Lawrence@gmail.com>
2023-05-29 15:56:07 -0400

    Slowing down mouse cursor movement

---
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/Ziewvater/config.h
@@ -0,0 +1,4 @@
+#define MK_KINETIC_SPEED                    // Enable Kinetic mode:  Uses a quadratic curve on cursor speed to allow precise movements at the beginning and increases speed thereafter.
+#define MOUSEKEY_MOVE_DELTA 7               // Step size for accelerating from initial to base speed (default: 8)
+#define MOUSEKEY_MOVE_MAX 100               // use instead of BASE SPEED to limit speed in Kinetic mode
+#define MOUSEKEY_INITIAL_SPEED 300          // Initial speed of the cursor in pixels per second (default: 100)
