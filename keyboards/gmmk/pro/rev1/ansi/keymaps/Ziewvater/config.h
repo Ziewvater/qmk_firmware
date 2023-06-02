371e939f6b9632dd65e441bd511da298c0f18cd5
Jeremy <Jeremy.Y.Lawrence@gmail.com>
2023-06-02 08:45:47 -0400

    Reconfigure mouse key speeds

--- keyboards/gmmk/pro/rev1/ansi/keymaps/Ziewvater/config.h
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/Ziewvater/config.h
@@ -1,4 +1,7 @@
 #define MK_KINETIC_SPEED                    // Enable Kinetic mode:  Uses a quadratic curve on cursor speed to allow precise movements at the beginning and increases speed thereafter.
-#define MOUSEKEY_MOVE_DELTA 7               // Step size for accelerating from initial to base speed (default: 8)
-#define MOUSEKEY_MOVE_MAX 100               // use instead of BASE SPEED to limit speed in Kinetic mode
+#define MOUSEKEY_INTERVAL 8                 // Time between cursor movements in milliseconds
+#define MOUSEKEY_MOVE_DELTA 36              // Step size for accelerating from initial to base speed (default: 16)
+#define MOUSEKEY_BASE_SPEED 3500            // Maximum cursor speed at which acceleration stops
+#define MOUSEKEY_DECELERATED_SPEED 600      // Decelerated cursor speed
+#define MOUSEKEY_ACCELERATED_SPEED 3500     // Accelerated cursor speed
 #define MOUSEKEY_INITIAL_SPEED 300          // Initial speed of the cursor in pixels per second (default: 100)
