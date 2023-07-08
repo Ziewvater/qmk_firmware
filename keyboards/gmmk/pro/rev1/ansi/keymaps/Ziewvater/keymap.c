5394f59742b10445b77856c2584f559d19877c0c
Jeremy <Jeremy.Y.Lawrence@gmail.com>
2023-07-07 22:34:56 -0400

    Fixing tapping on caps for esc

--- keyboards/gmmk/pro/rev1/ansi/keymaps/Ziewvater/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/Ziewvater/keymap.c
@@ -39,6 +39,11 @@ enum custom_keycodes {
     Z_META,
 };
 
+// Inline custom keycode definitions
+
+/// Using LT(0, CUSTOM_KEY) allows us to create both custom tap and hold behaviors for a single keycode
+#define LT_ZMT LT(0, Z_META)
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
     case Z_NEXT:
@@ -64,10 +69,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             SEND_STRING(SS_DOWN(X_RALT) SS_TAP(X_BACKSPACE) SS_UP(X_RALT));
         }
         break;
-    case Z_META:
+    case LT(0, Z_META): 
         if (record->tap.count && record->event.pressed) {
-            // Tapping key, just send Caps
-            SEND_STRING(SS_TAP(X_CAPS));
+            // Tapping key, just send Escape
+            SEND_STRING(SS_TAP(X_ESC));
         } else if (record->event.pressed) {
             // Holding key down
             if (get_mods() & MOD_MASK_GUI) {
@@ -84,6 +89,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             layer_off(_MFN);
             layer_off(_MVI);
         }
+        return false;
     }
     return true;
 };
@@ -124,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_BRID, KC_BRIU, KC_MCTL, KC_LPAD, KC_F5,   KC_F6,   KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, TG(_WIN),         KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_HOME,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_MCTL,
-        Z_META,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_DEL,
+        LT_ZMT,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_DEL,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
         KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, MO(_MFN),KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
